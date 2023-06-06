2620c774176478bac210cc4da538bb2745f11f81
Breno Brito <109661421+bbrtbr@users.noreply.github.com>
2023-06-06 19:22:59 -0300

    Alteração para modo genérico

--- lista.h
+++ lista.h
@@ -1,37 +1,21 @@
-#ifndef LISTA_H // IF para verificar se essa estruturação ja foi implementada
+#ifndef LISTA_H
 #define LISTA_H
 
-typedef struct No {
-    char pais[100];
-    char estado[100];
-    int totalCasos;
-    int totalCasosMS;
-    int naoConfirmadosPorMS;
-    int mortes;
-    int mortesMS;
-    char URL[200];
-    float mortes_por_100k_habitantes;
-    float totalCasos_por_100k_habitantes;
-    float mortes_por_totalCasos;
-    int recuperados;
-    int suspeitos;
-    int testes;
-    float testes_por_100k_habitantes;
-    int vacinados;
-    float vacinados_por_100_habitantes;
-    int vacinados_segunda_dose;
-    float vacinados_segunda_dose_por_100_habitantes;
-    int vacinados_dose_unica;
-    float vacinados_dose_unica_por_100_habitantes;
-    int vacinados_terceira_dose;
-    float vacinados_terceira_dose_por_100_habitantes;
-    char data[20];
-    int novosCasos;
-    int novasMortes;
-    struct No* proxTotalCasos;
-    struct No* proxMortes;
+typedef struct no {
+    void *dados;
+    struct no *anterior;
+    struct no *proximo;
 } No;
 
+typedef struct {
+    No *primeiro;
+    No *ultimo;
+    int tamanho;
+} Lista;
 
+Lista* criarLista();
+void destruirLista(Lista *lista);
+void inserirElemento(Lista *lista, void *dados, int tamanhoDados, int (*comparar)(void*, void*));
+void imprimirLista(Lista *lista, void (*imprimir)(void*));
 
-#endif  
+#endif
